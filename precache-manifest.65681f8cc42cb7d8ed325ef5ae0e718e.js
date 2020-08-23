2638ceca3eb4174ae81b6e70d55dfe25063f8986
Tarek Messaoudi <tmessaoudi94@gmail.com>
2020-08-23 23:43:16 +0200

    Updates

---
+++ precache-manifest.65681f8cc42cb7d8ed325ef5ae0e718e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80316c7303f33e2d854473ec679d2710",
+    "url": "/henri-potier-library/index.html"
+  },
+  {
+    "revision": "e0dd31bfb857674ffed5",
+    "url": "/henri-potier-library/static/css/main.5d261fc1.chunk.css"
+  },
+  {
+    "revision": "5da4681d51a074cc69fc",
+    "url": "/henri-potier-library/static/js/2.25cc3bfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/henri-potier-library/static/js/2.25cc3bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0dd31bfb857674ffed5",
+    "url": "/henri-potier-library/static/js/main.923711ab.chunk.js"
+  },
+  {
+    "revision": "703417b5f99638829a2d",
+    "url": "/henri-potier-library/static/js/runtime-main.edbfa3b6.js"
+  }
+]);
